f0bcb996cda94b790f3d9c2938aef04c76740fb1
Pieter Gunst <pietergunst@gmail.com>
2016-03-21 00:19:41 -0700

    make tagging dependent on company to keep our data clean

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ActiveRecord::Base
-  has_many :taggings
+  has_many :taggings,  dependent: :destroy
   has_many :tags, through: :taggings
   
   #this should be has_one, but apparently there's a known bug
