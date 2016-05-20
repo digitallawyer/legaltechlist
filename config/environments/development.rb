8ac9243c6ab31f46dc9df4f4d14df0f165cedc9d
Pieter Gunst <pietergunst@gmail.com>
2016-05-19 23:09:53 -0700

    config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,8 +20,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   ActionMailer::Base.smtp_settings = {
-  :user_name => ENV["SENDGRID_SECRET_DEV"],
-  :password => ENV["SENDGRID_SECRET_DEV"],
+  :user_name => ENV["SENDGRID_USERNAME"],
+  :password => ENV["SENDGRID_PASSWORD"],
   :domain => 'localhost',
   :address => 'smtp.sendgrid.net',
   :port => 587,
