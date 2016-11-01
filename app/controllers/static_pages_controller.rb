f48bcb92336652416fd2cfaf9eae9179d1a5dabe
Patrick Häde <patrick.haede@wunderfactory.de>
2016-10-31 17:53:44 -0700

    stats are displayed in relation to year

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,6 +7,12 @@ class StaticPagesController < ApplicationController
   end
 
   def statistics
+  	if params[:founded_date]
+  		@companies = Company.where('founded_date' => params[:founded_date]).all
+  	else
+  		@companies = Company.all
+  	end
+  	@years = ['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016']
   end
 
 end
