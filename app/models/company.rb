b492d3f23b563dca0f4cb91aa04864dc4f9b5ba3
johnwang21 <johnwang21@gmail.com>
2020-09-11 15:44:02 -0700

    cleaned up spread sheet

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,21 +4,21 @@ class Company < ActiveRecord::Base
 
   has_many :taggings,  dependent: :destroy
   has_many :tags, through: :taggings
-  
+
   #this should be has_one, but apparently there's a known bug
   belongs_to :category
   belongs_to :sub_category
   belongs_to :business_model
   belongs_to :target_client
-  
+
   # I'm not 100% sure these are required.
   accepts_nested_attributes_for :category
   accepts_nested_attributes_for :sub_category
   accepts_nested_attributes_for :business_model
   accepts_nested_attributes_for :target_client
-  
+
   # Validation for manual entry of data.
-  validates :name, presence: true, length: {minimum: 3}
+  validates :name, presence: true, length: {minimum: 2}
   validates :location, presence: true, length: {minimum: 1}
   validates :founded_date, presence: true, format: {with: /\d\d\d\d/, message: "must be a 4-digit year."}
   validates :category, presence: true
@@ -30,7 +30,7 @@ class Company < ActiveRecord::Base
 
   geocoded_by :location
   after_validation :geocode, :if => :location_changed?
-  
+
 	include PgSearch
 	pg_search_scope :search,
                   	:against => :name,
@@ -45,22 +45,22 @@ class Company < ActiveRecord::Base
 			all
 		end
 	end
-  
+
   def self.tagged_with(name)
     Tag.find_by_name!(name).companies
   end
-  
+
   def all_tags=(names)
     self.tags = names.split(",").map do |name|
       Tag.where(name: name.strip.downcase).first_or_create!
     end
   end
-  
+
   def all_tags
     self.tags.map(&:name).join(", ")
   end
 
-  #convenience method to parse the twitter username from the twitter url, 
+  #convenience method to parse the twitter username from the twitter url,
   #but also handle the case where the user entered their twitter name instead.
   def twitter_name
     #parse the twitter url to get the twitter_name
@@ -71,7 +71,7 @@ class Company < ActiveRecord::Base
         else
           self.twitter_url.split('twitter.com/').last
         end
-      else 
+      else
         if self.twitter_url.include? "@"
           self.twitter_url.split('@').last
         else
@@ -103,7 +103,7 @@ class Company < ActiveRecord::Base
       end
     end
   end
-  
+
   def publish_to_list
     if (self.visible? && Rails.configuration.twitter_publish)
       #initialize twitter_client to access their API
