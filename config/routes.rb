eaf4f41cfb6c5d7703bf90a7de27ada2fe784f42
johnwang21 <johnwang21@gmail.com>
2021-01-24 16:09:08 -0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
  # get 'sub_category/:category' => 'sub_category#index', as: :category
     
   get 'static_pages/home'
-  
+  get 'admin/pieter', to: 'admin/pieter#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
