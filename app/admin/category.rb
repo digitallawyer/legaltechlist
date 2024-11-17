43fe0bf6b940764d14ddf32c777c8982795666db
Pieter Gunst <pieter@legal.io>
2024-11-17 15:03:55 -0800

    activeadmin params

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,30 +1,35 @@
 ActiveAdmin.register Category do
-  permit_params :name
 
-  index do
-    selectable_column
-    id_column
-    column :name
-    column :created_at
-    actions
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :name, :description
+
+index do
+	column :name
+	column :description
+  actions
+end
+
+# Remove any default filters that might be trying to use companies association
+remove_filter :companies
+
+form do |f|
+  f.inputs do
+    f.input :name
+    f.input :description
   end
+  f.actions
+end
 
-  filter :name
-  filter :created_at
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
 
-  form do |f|
-    f.inputs do
-      f.input :name
-    end
-    f.actions
-  end
 
-  show do
-    attributes_table do
-      row :id
-      row :name
-      row :created_at
-      row :updated_at
-    end
-  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,18 @@
-class Category < ApplicationRecord
+class Category < ActiveRecord::Base
+  has_many :companies
   has_many :sub_categories
-  has_many :companies, through: :sub_categories
+
+  accepts_nested_attributes_for :companies
   
+  def sub_category
+    SubCategory.where(:category => id)
+  end
+
   def self.ransackable_attributes(auth_object = nil)
-    %w[id name created_at updated_at]
+    ["created_at", "description", "id", "name", "updated_at"]
   end
 
   def self.ransackable_associations(auth_object = nil)
-    %w[sub_categories companies]
+    ["companies"]
   end
 end
